81b4a6f5bd383fa4444ef35bf38d365ffe8683a7
andrey <a.shahov.garlic@yandex.ru>
2024-03-20 10:52:59 +0500

    initial commit

---
+++ install.sh
@@ -0,0 +1,71 @@
+#!/usr/bin/env bash
+
+# Script for installing desktop environment for python/C/C++
+# development on bare Debian 12 bookworm
+
+URL_DOTFILES='https://github.com/a-shahov/dotfiles.git'
+
+display_usage() {
+    echo 'Usage : install.sh -u <username> -p <password>'
+    exit 1
+}
+
+if [[ "$(id -u)" != 0 ]] then
+    echo 'script requires root privileges'
+    exit 1
+fi
+
+if [[ -z $4 ]] then
+    display_usage
+fi
+
+while [[ "$1" != "" ]]; do
+    case $1 in
+	-u) shift 1; USERNAME=$1 ;;
+	-p) shift 1; PASSWORD=$1 ;;
+    esac
+    shift 1
+done
+
+if [[ "$PASSWORD" == "" ]] || [[ "$USERNAME" == "" ]] then
+    display_usage
+fi
+
+HOME_DIR=/home/$USERNAME
+
+create_user() {
+# Update system and Install base packages
+apt-get update && apt-get dist-upgrade -y
+apt-get install -y sudo zsh git
+
+# Creating user
+mkdir -p $HOME_DIR/.config \
+	 $HOME_DIR/.local/bin \
+	 $HOME_DIR/.local/share \
+	 $HOME_DIR/.local/state \
+	 $HOME_DIR/.cache \
+	 $HOME_DIR/.ssh \
+	 $HOME_DIR/downloads
+
+useradd --home-dir $HOME_DIR --groups sudo --shell /usr/bin/zsh $USERNAME
+echo "$USERNAME:$PASSWORD" | chpasswd 
+chown -R $USERNAME:$USERNAME $HOME_DIR
+
+# generate ssh keys
+sudo -u $USERNAME ssh-keygen -t ed25519 -N "" -f $HOME_DIR/.ssh/id_ed25519 <<< $'\ny'
+}
+# НЕ РАБОТАЕТ ГОВНО А ТАКЖЕ НУЖНО У ДОТФАЙЛОВ ПОМЕНЯТЬ ПЕРМИШОНЫ!!!
+
+install_dotfiles() {
+# Install dotfiles
+git clone $URL_DOTFILES
+mv $(pwd)/dotfiles $HOME_DIR/
+chown -R $USERNAME:$USERNAME $HOME_DIR/dotfiles
+sudo -u $USERNAME $HOME_DIR/dotfiles/install
+}
+
+create_user
+install_dotfiles
+
+#apt-get install -y zsh neovim tmux gtypist wget\
+                   #bat ffmpeg curl
