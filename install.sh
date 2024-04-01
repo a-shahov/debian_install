9f55cd0bd9282330dcc6f6eef92e19571c1df8dd
andrey <a.shahov.garlic@yandex.ru>
2024-04-01 17:29:51 +0500

    install dotfiles

--- install.sh
+++ install.sh
@@ -31,12 +31,12 @@ if [[ "$PASSWORD" == "" ]] || [[ "$USERNAME" == "" ]] then
     display_usage
 fi
 
-HOME_DIR=/home/$USERNAME
+HOME_DIR="/home/$USERNAME"
 
 create_user() {
 # Update system and Install base packages
 apt-get update && apt-get dist-upgrade -y
-apt-get install -y sudo zsh git
+apt-get install -y sudo zsh git zip unzip
 
 # Creating user
 mkdir -p $HOME_DIR/.config \
@@ -54,18 +54,21 @@ chown -R $USERNAME:$USERNAME $HOME_DIR
 # generate ssh keys
 sudo -u $USERNAME ssh-keygen -t ed25519 -N "" -f $HOME_DIR/.ssh/id_ed25519 <<< $'\ny'
 }
-# НЕ РАБОТАЕТ ГОВНО А ТАКЖЕ НУЖНО У ДОТФАЙЛОВ ПОМЕНЯТЬ ПЕРМИШОНЫ!!!
 
 install_dotfiles() {
 # Install dotfiles
 git clone $URL_DOTFILES
 mv $(pwd)/dotfiles $HOME_DIR/
 chown -R $USERNAME:$USERNAME $HOME_DIR/dotfiles
-sudo -u $USERNAME $HOME_DIR/dotfiles/install
+sudo -u $USERNAME $HOME_DIR/dotfiles/install -c user.conf.yaml
+$HOMEDIR/dotfiles/install -c admin.conf.yaml
 }
 
 create_user
 install_dotfiles
 
-#apt-get install -y zsh neovim tmux gtypist wget\
-                   #bat ffmpeg curl
+# Qtile dependecies
+apt-get install xorg libpangocairo-1.0-0 libxcb1 libcairo2 libgdk-pixbuf-2.0-0 python3-pip python3-venv
+
+#apt-get install -y zsh neovim tmux gtypist wget xorg-x\
+                   #bat ffmpeg curl build-essential xorg
